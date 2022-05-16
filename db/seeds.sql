e6f731526b567daaa074ae75ea64ab553c2ce8b3
Anita Chavez <anita.chavez@carlsbadschools.net>
2022-05-15 20:47:29 -0600

    working on index.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,33 +1,34 @@
 INSERT INTO departments (dept_name)
-VALUES ("MANAGEMENT"),
-("ENGINEERING"),
+VALUES ("NETWORK"),
+("SYSTEMS"),
+("DIGITAL MEDIA")
 ("IT");
 
 INSERT INTO roles (title, salary, dept_id)
-VALUES ("Director of Technology", 90000.00, 1),
-("Sr. Network Engineer", 80000, 2),
-("Jr. Network Enginerer", 65000, 2),
-("Sr. Systems Engineer", 80000, 2),
-("Jr. Systems Engineer", 65000, 2),
+VALUES ("Director of Technology", 90000, 1),
+("Sr. Network Engineer", 80000, 4),
+("Jr. Network Enginerer", 65000, 4),
+("Sr. Systems Engineer", 80000, 3),
+("Jr. Systems Engineer", 65000, 3),
 ("Sr. Digital Media Engineer", 80000, 2),
 ("Jr. Digital Media Engineer" 65000, 2),
-("Technical Support Supervisor", 70000, 1)
-("Technical Specialist", 50000, 3),
-("Information Technician", 40000, 3);
+("Technical Support Supervisor", 70000, 5)
+("Technical Specialist", 50000, 5),
+("Information Technician", 40000, 5);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("Neeta", "Chavez", 20, ),
-("Jack", "Skellington", 19, ),
-("Oogie", "Boogie", 18, ),
-("Cheshire", "Cat", 17, ),
-("Mad", "Hatter", 16, ),
-("Korben", "Dallas", 15, ),
-("Leeloo", "Dallas", 14, ),
-("Clark", "Devereaux", 13, ),
-("Mikey", "Walsh", 12, ),
-("Neo", "Anderson", 11, ),
-("Agent", "Smith", 10, ),
-("Star", "Lord", 9, null),
-("Rocket", "Raccoon", 8, ),
-("Ingigo", "Montoya", 7, ),
-("Miracle", "Max", 6, ),
+VALUES ("Neeta", "Chavez", 6, null),
+("Star", "Lord", 6, 5),
+("Mad", "Hatter", 2, null),
+("Mikey", "Walsh", 2, 3),
+("Korben", "Dallas", 8, null),
+("Clark", "Devereaux", 8, 7),
+("Oogie", "Boogie", 4, null),
+("Cheshire", "Cat", 4, 9),
+("Leeloo", "Dallas", 14, null),
+("Neo", "Anderson", 12, 11),
+("Agent", "Smith", 12, 11),
+("Rocket", "Raccoon", 10, 11),
+("Inigo", "Montoya", 10, 11),
+("Miracle", "Max", 10, 11),
+("Jack", "Skellington", 1, null),
--- index.js
+++ index.js
@@ -19,10 +19,11 @@ const db = mysql.createConnection(
 
 createConnection.connect(err => {
     if (err) throw err;
-    console.log('connected as id ' + connection.threadID);
+    console.log('connected');
     afterConnection();
 });
 
+//welcome Image
 afterConnection = () => {
     console.log("********************************")
     console.log("*                              *")
@@ -30,4 +31,143 @@ afterConnection = () => {
     console.log("*  Employee Management System  *")
     console.log("*                              *")
     console.log("********************************")
+    startPrompts();
+};
+
+//starts prompts
+const startPrompts = () => {
+    inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'Please choose one of the following options.',
+            choice: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Quit'
+            ]
+        }
+    ])
+
+//Switch for options 
+.then((response) => {
+    switch (response.choice) {
+        case 'View all departments':
+            viewDepartments();
+            break;
+        case 'View all Roles':
+            viewRoles();
+            break;
+        case 'View all employees':
+            viewEmployees();
+            break;
+        case 'Add a department':
+            addDepartment();
+            break;    
+        case 'Add a role':
+            addRole();
+            break;
+        case 'Add an Employee':
+            addEmployee();
+            break;
+        case 'Update a Role':
+            updateRole();
+            break;
+        case 'Quit':
+            quit();
+            break;    
+    }
+})
 }
+//Function to view Departments
+function viewDepartments() {
+    const request = "SELECT * FROM departments";
+    db.query(request, function(err, res) {
+        if (err) throw err;
+        console.log("Viewing all Departments");
+        console.table(res);
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'choice',
+                message: 'Please choose an option.',
+                choices: [
+                    'Main Menu',
+                ]
+            }
+        ])
+        .then((response) => {
+            switch (response.choice) {
+                case 'Main Menu':
+                startPrompts();
+                break;
+                case 'Quit':
+                Quit();
+            }
+        })
+    })
+}
+
+//Function to view all roles
+function viewRoles() {
+    const request = "SELECT * FROM roles";
+    db.query(request, function(err, res) {
+        if (err) throw err;
+        console.log("Viewing all Roles");
+        console.table(res);
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'choice',
+                message: 'Please choose an option.',
+                choices: [
+                    'Main Menu',
+                ]
+            }
+        ])
+        .then((response) => {
+            switch (response.choice) {
+                case 'Main Menu':
+                startPrompts();
+                break;
+                case 'Quit':
+                Quit();
+            }
+        })
+    })
+}
+
+//Function to view all Employees
+function viewEmployees() {
+    const request = "SELECT * FROM employees";
+    db.query(request, function(err, res) {
+        if (err) throw err;
+        console.log("Viewing all Departments");
+        console.table(res);
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'choice',
+                message: 'Please choose an option.',
+                choices: [
+                    'Main Menu',
+                ]
+            }
+        ])
+        .then((response) => {
+            switch (response.choice) {
+                case 'Main Menu':
+                startPrompts();
+                break;
+                case 'Quit':
+                Quit();
+            }
+        })
+    })
+}
+
