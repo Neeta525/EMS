f0f9d6ad4327dffc3e0c5571c29db2f476c9dd50
Anita Chavez <chaveza@carlsbad.k12.nm.us>
2022-05-20 15:03:01 -0600

    new changes

--- index.js
+++ index.js
@@ -171,3 +171,123 @@ function viewEmployees() {
     })
 }
 
+//function to add a new department
+async function addDepartment() {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'Please enter the name of the department you are adding.'
+    }])
+    .then((response) => {
+        addDataTo('departments', response);
+    });
+}
+
+//function to add a role
+async function addRole() {
+    inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Please enter the name of the role you are adding.'
+    },
+    {
+        type: 'input',
+        name: 'salary',
+        message: 'Please enter the salary for this new role.'
+    },
+    {
+        type: 'list',
+        name: 'department',
+        message: 'Please choose the department for this new role.',
+        choices: [
+            'Network',
+            'Systems',
+            'Digital Media',
+            'IT'
+    ]    
+    }     
+])
+    .then((response) => {
+        addDataTo('roles', response);
+    });
+}
+
+//function to add an employee
+async function addEmployee() {
+    inquirer.prompt([
+    {
+        type: 'input',
+        name: 'firstName',
+        message: 'Please enter the first name of the employee you are adding.'
+    },
+    {
+        type: 'input',
+        name: 'LastName',
+        message: 'Please enter the last name of the employee you are adding.'
+    },
+    {
+        type: 'list',
+        name: 'role',
+        message: 'Please choose the role for this employee.',
+        choices: [
+            'Sr. Network Engineer',
+            'Jr. Network Enginerer',
+            'Sr. Systems Enginerer',
+            'Jr. Systems Engineer',
+            'Sr. Digital Media Engineer',
+            'Jr. Digital Media Engineer',
+            'Technical Support Supervisor',
+            'Technical Specialist',
+            'Information Technician'
+        ]
+    },
+    {
+        type: 'list',
+        name: 'department',
+        message: 'Please choose the department for this new role.',
+        choices: [
+            'Network',
+            'Systems',
+            'Digital Media',
+            'IT'
+    ]    
+    }     
+])
+    .then((response) => {
+        addDataTo('employees', response);
+    });
+}
+
+//function to update a role
+async function updateRole() {
+    inquirer.prompt([
+    {
+        type: 'list',
+        name: 'employeeNames',
+        message: 'Please select the employee you would like to update',
+        choice: 
+    },   
+    {
+        type: 'list',
+        name: 'role',
+        message: 'Please choose their new role.',
+        choices: [
+            'Sr. Network Engineer',
+            'Jr. Network Enginerer',
+            'Sr. Systems Enginerer',
+            'Jr. Systems Engineer',
+            'Sr. Digital Media Engineer',
+            'Jr. Digital Media Engineer',
+            'Technical Support Supervisor',
+            'Technical Specialist',
+            'Information Technician'
+        ]
+    }  
+])
+    .then((response) => {
+        addDataTo('employees', response);
+    });
+}
+
+startPrompts();
